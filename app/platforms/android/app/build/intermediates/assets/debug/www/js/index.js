4ef9912d3f96d5d451df6f5bb566bc08cb3ee553
William Barteck <wbarteck@gmail.com>
2018-11-03 16:32:06 -0400

    cordova app

---
+++ app/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -0,0 +1,24 @@
+var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('pause', this.onPause, false);
+        document.addEventListener('resume', this.onResume, false);
+        document.addEventListener('backbutton', this.onBackButton, false);
+    },
+    onDeviceReady: function() {
+        navigator.notification.alert('test', null, 'Test Alert', 'done');
+    },
+    onPause: function() {},
+    onResume: function() {},
+    onBackButton: function() {
+        navigator.notification.beep(2);
+        navigator.notification.alert('back', null, 'Test Back', 'done');
+    }
+}
+
+
+
+app.initialize();
---
+++ app/platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,42 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/browser/notification.js",
+        "id": "cordova-plugin-dialogs.notification_browser",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/src/browser/DeviceProxy.js",
+        "id": "cordova-plugin-device.DeviceProxy",
+        "pluginId": "cordova-plugin-device",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-dialogs": "2.0.1",
+    "cordova-plugin-device": "2.0.2"
+}
+// BOTTOM OF METADATA
+});
