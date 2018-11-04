f18f29c49e719640f9b0dfb65c786e0afb46dc37
William Barteck <wbarteck@gmail.com>
2018-11-04 17:53:12 -0500

    App layout

--- app/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
+++ app/platforms/android/app/build/intermediates/assets/debug/www/js/index.js
@@ -9,16 +9,33 @@ var app = {
         document.addEventListener('backbutton', this.onBackButton, false);
     },
     onDeviceReady: function() {
-        navigator.notification.alert('test', null, 'Test Alert', 'done');
+        //navigator.notification.alert('test', null, 'Test Alert', 'done');
     },
     onPause: function() {},
     onResume: function() {},
     onBackButton: function() {
-        navigator.notification.beep(2);
         navigator.notification.alert('back', null, 'Test Back', 'done');
     }
 }
 
-
-
 app.initialize();
+
+$(document).ready(function() {
+    var title = $('#header-title');
+
+    $('.carousel').carousel('pause');
+
+    $('.carousel-inner').on('swipeleft', function(e) {
+        $(this).parent().carousel('next');
+    });
+    $('.carousel-inner').on('swiperight', function(e) {
+        $(this).parent().carousel('prev');
+    });
+
+    $('#pageCarousel').on('slid.bs.carousel', function (e) {
+        var page = $('.item.active').attr('id');
+        title.html(jsUcfirst(page));
+    });
+});
+
+function jsUcfirst(string) { return string.charAt(0).toUpperCase() + string.slice(1);}
--- app/www/js/index.js
+++ app/www/js/index.js
@@ -9,7 +9,7 @@ var app = {
         document.addEventListener('backbutton', this.onBackButton, false);
     },
     onDeviceReady: function() {
-        navigator.notification.alert('test', null, 'Test Alert', 'done');
+        //navigator.notification.alert('test', null, 'Test Alert', 'done');
     },
     onPause: function() {},
     onResume: function() {},
@@ -18,6 +18,24 @@ var app = {
     }
 }
 
-
-
 app.initialize();
+
+$(document).ready(function() {
+    var title = $('#header-title');
+
+    $('.carousel').carousel('pause');
+
+    $('.carousel-inner').on('swipeleft', function(e) {
+        $(this).parent().carousel('next');
+    });
+    $('.carousel-inner').on('swiperight', function(e) {
+        $(this).parent().carousel('prev');
+    });
+
+    $('#pageCarousel').on('slid.bs.carousel', function (e) {
+        var page = $('.item.active').attr('id');
+        title.html(jsUcfirst(page));
+    });
+});
+
+function jsUcfirst(string) { return string.charAt(0).toUpperCase() + string.slice(1);}
